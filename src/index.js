db56aeb51164d83b9a38be86f3e99b045b9eefc1
renersullivan <renersullivan1@gmail.com>
2022-07-16 17:30:43 -0300

    projeto novo page nubank

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import './style/global.css';
+//import Home from './pages/Home';
+import Consulta from './pages/consulta/Consulta';
+
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   
+    <Consulta/>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import './Home.css'
+function Home(){
+    return(
+             
+        <div className='home'>   
+           <div className="title">Cartão de crédito com anuidade zero. Conta digital prática e sem custos.</div>
+           <div className='card-login'>
+             <p>Com a Nubank a resposta vem em menos de 1 minuto</p>   
+             <input className='input-card' type="tel" placeholder='Digite seu CPF.' />
+             <div className='btn-submit'>Continar </div>
+               
+            </div>
+           
+
+            
+        </div>
+
+
+    )
+}
+export default Home;
