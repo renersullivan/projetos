e1eae4146bf40ebd95d02a0293c75fa472d79fff
renersullivan <renersullivan1@gmail.com>
2022-07-28 18:15:37 -0300

    alteracoes nos container mais regularizacao dos tamanhos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import './style/global.css';
-//import Home from './pages/Home';
+import Home from './pages/home/Home';
 import Consulta from './pages/consulta/Consulta';
 
 
@@ -13,9 +17,15 @@ import Consulta from './pages/consulta/Consulta';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Consulta/>
-  </React.StrictMode>
+<BrowserRouter>
+  <Routes>
+      
+   <Route path="/home" element={<Home/>} />
+   <Route path='/consulta' element={<Consulta/>}/>
+      
+      
+  </Routes>
+</BrowserRouter>
   
 );
 
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,7 +3,9 @@ function Home(){
     return(
              
         <div className='home'>   
-           <div className='logo-nubank'><img src='/home/rener/Dev/PROJETOS/nunbank/website/nunbank/public/kisspng-nubank-credit-card-financial-crisis-of-200708-brazil-png-5b19b73c3f6738.9154852715284119642597.png' alt="Logo NubanK."/></div>
+            <div className='logo-nubank'>
+                {/* <img src='public/logo192.png'/> */}
+            </div>
            <div className="title">Cartão de crédito com anuidade zero. Conta digital prática e sem custos.</div>
            <div className='card-login'>
              <p>Com a Nubank a resposta vem em menos de 1 minuto</p>   
