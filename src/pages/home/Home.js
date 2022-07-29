2c426ed4c7103082133a1505cd1e592e1257a846
renersullivan <renersullivan1@gmail.com>
2022-07-29 19:49:58 -0300

    adicionando div na page home para igualar os titulos

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,15 +4,18 @@ function Home(){
              
         <div className='home'>   
             <div className='logo-nubank'>
-                {/* <img src='public/logo192.png'/> */}
+                
             </div>
-           <div className="title">Cartão de crédito com anuidade zero. Conta digital prática e sem custos.</div>
-           <div className='card-login'>
-             <p>Com a Nubank a resposta vem em menos de 1 minuto</p>   
-             <input className='input-card' type="tel" placeholder='Digite seu CPF.' />
-             <div className='btn-submit'>Continar </div>
-               
+            <div className='container-home'>
+                <div className="title">Cartão de crédito com anuidade zero. Conta digital prática e sem custos.</div>
+                 <div className='card-login'>
+                <p>Com a Nubank a resposta vem em menos de 1 minuto</p>   
+                <input className='input-card' type="tel" placeholder='Digite seu CPF.' />
+                <div className='btn-submit'>Continar </div>
+                
+                </div>
             </div>
+
            
 
             
